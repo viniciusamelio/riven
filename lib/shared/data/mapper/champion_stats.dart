6e76996acf5df321e6a6554e83aed77531c754d0
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-08-07 15:08:54 -0300

    fix string convertion

--- lib/shared/data/mapper/champion_stats.dart
+++ lib/shared/data/mapper/champion_stats.dart
@@ -4,20 +4,20 @@ class ChampionStatsMapper {
   static ChampionStats fromMap(Map<String, dynamic> map) {
     return ChampionStats(
       banrate: double.parse(
-        map['banrate'].toString().replaceAll('%', '').replaceAll('.', ''),
+        map['banrate'].replaceAll('%', ''),
       ),
       gamesAnalyzed: int.parse(
         map['gamesAnalyzed'].toString().replaceAll(',', ''),
       ),
       name: map['name'],
       pickrate: double.parse(
-        map['pickrate'].toString().replaceAll('%', '').replaceAll('.', ''),
+        map['pickrate'].replaceAll('%', ''),
       ),
       portraitUrl: map['portrait'],
       role: map['role'],
       tier: map['tier'],
       winrate: double.parse(
-        map['winrate'].toString().replaceAll('%', '').replaceAll('.', ''),
+        map['winrate'].toString().replaceAll('%', ''),
       ),
     );
   }
