90ba5dcee2efbe352ec7b4a91bce6582bab037f1
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-08-07 15:06:52 -0300

    Adding new params

--- lib/shared/presentation/widgets/params/portrait_grid.dart
+++ lib/shared/presentation/widgets/params/portrait_grid.dart
@@ -1,13 +1,18 @@
+import 'package:flutter/widgets.dart';
 import 'package:riven/shared/domain/entities/build.dart';
 
 class PortraitGridParams {
+  String? routeName;
   int? maxCrossAxisExtent;
   final List<Build> builds;
   double? portraitsSize;
+  Color? accentColor;
 
   PortraitGridParams(
     this.builds, {
     this.maxCrossAxisExtent,
+    this.routeName,
+    this.accentColor,
     this.portraitsSize,
   });
 }
