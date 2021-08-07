2102019217a554885709a28031d569566f845324
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-08-07 15:06:41 -0300

    Adding nem options

--- lib/shared/presentation/widgets/params/portraits.dart
+++ lib/shared/presentation/widgets/params/portraits.dart
@@ -1,18 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:riven/shared/presentation/styles/color.dart';
 
 class PortraitParams {
   Function()? onTap;
   final String imageUrl;
   String? name;
   String? lane;
-  final Color accentColor;
+  Color? accentColor;
+  Color? textColor;
   double? size;
+  double? borderWidth;
 
-  PortraitParams(
-      {this.onTap,
-      required this.imageUrl,
-      this.name,
-      this.lane,
-      required this.accentColor,
-      size});
+  PortraitParams({
+    this.onTap,
+    required this.imageUrl,
+    this.name,
+    this.lane,
+    this.accentColor,
+    this.size,
+    this.borderWidth,
+  }) {
+    textColor = accentColor;
+    if (accentColor == Colors.transparent) {
+      textColor = primaryGreen;
+    }
+  }
 }
