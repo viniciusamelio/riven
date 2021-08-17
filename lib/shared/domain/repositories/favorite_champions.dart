c8c53195dfc96ec0b674eefe372be77dcebce066
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-08-17 20:57:48 -0300

    fix remove method's signature

--- lib/shared/domain/repositories/favorite_champions.dart
+++ lib/shared/domain/repositories/favorite_champions.dart
@@ -3,6 +3,6 @@ import 'package:riven/shared/domain/entities/build.dart';
 abstract class FavoriteChampionsRepository {
   Future<List<Build>> list();
   Future<Build> search(String key);
-  void remove();
+  void remove(String key);
   void save(String key, Build value);
 }
