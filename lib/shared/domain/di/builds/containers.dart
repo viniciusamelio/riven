3424ccaefc8c19468346612284b8e5136e3a09ca
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-08-30 18:59:10 -0300

    Adding getFavoriteChampions DI Container

--- lib/shared/domain/di/builds/containers.dart
+++ lib/shared/domain/di/builds/containers.dart
@@ -1,10 +1,16 @@
 import 'package:kiwi/kiwi.dart';
 import 'package:riven/shared/data/repositories/builds.dart';
+import 'package:riven/shared/data/repositories/favorite_champions.dart';
 import 'package:riven/shared/domain/datasources/document_db.dart';
+import 'package:riven/shared/domain/datasources/local_storage.dart';
 import 'package:riven/shared/domain/repositories/build.dart';
+import 'package:riven/shared/domain/repositories/favorite_champions.dart';
 import 'package:riven/shared/domain/use_cases/builds/get_builds.dart';
 import 'package:riven/shared/domain/use_cases/builds/iget_builds_use_case.dart';
+import 'package:riven/shared/domain/use_cases/favorite_champions/get_favorite_champions.dart';
+import 'package:riven/shared/domain/use_cases/favorite_champions/iget_favorite_champions_use_case.dart';
 import 'package:riven/shared/external/firebase/firestore.dart';
+import 'package:riven/shared/external/get/get_storage.dart';
 
 final getBuildsDIContainer = KiwiContainer()
   ..registerFactory<IGetBuildsUseCase>(
@@ -19,4 +25,19 @@ final getBuildsDIContainer = KiwiContainer()
   )
   ..registerFactory<DocumentDBDatasource>(
     (container) => FirestoreDatasource(collectionName: 'builds'),
+  );
+
+final getFavoriteChampionsDIContainer = KiwiContainer()
+  ..registerFactory<IGetFavoriteChampionsUseCase>(
+    (container) => GetFavoriteChampions(
+      container.resolve(),
+    ),
+  )
+  ..registerFactory<FavoriteChampionsRepository>(
+    (container) => FavoriteChampionsRepositoryImpl(
+      container.resolve(),
+    ),
+  )
+  ..registerFactory<LocalStorage>(
+    (container) => GetStorageDataSource(),
   );
