bb377b51df906f97a8c91b56973968f406bb676b
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-02 20:12:10 -0300

    Using kiwi container instance for route observing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:provider/provider.dart';
 import 'package:riven/modules/builds/presentation/views/build.dart';
@@ -9,6 +8,8 @@ import 'package:riven/modules/home/presentation/views/loading.dart';
 import 'package:riven/shared/domain/entities/build_set.dart';
 import 'package:riven/shared/presentation/styles/theme.dart';
 
+import 'shared/domain/di/builds/containers.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -16,14 +17,12 @@ void main() async {
   runApp(MyApp());
 }
 
-final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+// final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 class MyApp extends StatelessWidget {
-  final routeObserver = Get.put<RouteObserver>(
-    RouteObserver<PageRoute>(),
-  );
   @override
   Widget build(BuildContext context) {
+    RouteObserver routeObserver = routeObserverDIContainer();
     return MultiProvider(
       providers: [
         Provider<BuildSet>(
