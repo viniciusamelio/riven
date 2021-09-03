551c76709ceb0671326b384569da60a326483206
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-02 21:05:38 -0300

    Adding input with autocomplete feature to search page

--- lib/modules/search/presentation/views/search.dart
+++ lib/modules/search/presentation/views/search.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:from_css_color/from_css_color.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:riven/shared/domain/entities/build.dart';
+import 'package:riven/shared/domain/entities/build_set.dart';
 import 'package:riven/shared/presentation/styles/color.dart';
+import 'package:riven/shared/presentation/styles/text.dart';
+import 'package:riven/shared/presentation/widgets/molecules/portraits.dart';
+import 'package:riven/shared/presentation/widgets/params/portraits.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -11,6 +18,14 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchState extends State<SearchScreen> {
+  late List<Build> _builds;
+
+  @override
+  void initState() {
+    _builds = Provider.of<BuildSet>(context, listen: false).builds;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -35,34 +50,63 @@ class _SearchState extends State<SearchScreen> {
               ),
             ),
             const SizedBox(height: 20),
-            TextField(
-              onChanged: (value) => value,
-              decoration: InputDecoration(
-                labelText: 'Nome do campeão',
-                suffix: InkWell(
-                  onTap: () => print(
-                    'Action',
-                  ),
-                  child: Container(
-                    height: 30,
-                    width: 30,
-                    decoration: BoxDecoration(
-                      color: fromCssColor('#242631'),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(
-                          8,
-                        ),
-                      ),
-                    ),
-                    child: Center(
-                      child: Icon(
-                        Icons.search,
-                        color: primaryGreen,
-                      ),
-                    ),
-                  ),
-                ),
+            TypeAheadField(
+              textFieldConfiguration: TextFieldConfiguration(
+                cursorColor: primaryGreen,
+                decoration: InputDecoration(
+                    labelText: 'Nome do campeão',
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 24,
+                      horizontal: 12,
+                    )),
               ),
+              suggestionsCallback: (pattern) {
+                return _builds.where(
+                  (element) => element.champion!.name.contains(pattern),
+                );
+              },
+              suggestionsBoxDecoration: SuggestionsBoxDecoration(
+                color: fromCssColor('#242631'),
+                borderRadius: BorderRadius.circular(4),
+                elevation: 0,
+              ),
+              noItemsFoundBuilder: (context) {
+                return ListTile(
+                  title: Text(
+                    'Campeão não encontrado',
+                    style: assetLabelTextStyle(
+                      color: red,
+                    ),
+                  ),
+                );
+              },
+              itemBuilder: (context, Build suggestion) {
+                return ListTile(
+                  tileColor: fromCssColor('#242631').withOpacity(0.8),
+                  leading: Container(
+                    width: 40,
+                    child: Portrait(
+                      data: PortraitParams(
+                        imageUrl: suggestion.champion!.portraitUrl,
+                        size: 40,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    suggestion.champion!.name,
+                    style: assetLabelTextStyle(
+                      color: Colors.white.withAlpha(200),
+                    ),
+                  ),
+                  subtitle: Text(
+                    suggestion.champion!.role,
+                    style: assetLabelTextStyle(),
+                  ),
+                );
+              },
+              onSuggestionSelected: (suggestion) {
+                print('suggestion selected');
+              },
             ),
             const SizedBox(height: 20),
             Row(
