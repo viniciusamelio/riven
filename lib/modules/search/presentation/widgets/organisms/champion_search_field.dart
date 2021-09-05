1f80be8686cc33b3af0647af315fc26416adfa8a
Vinicius Amélio <vinicius.amelio@zoho.com>
2021-09-05 20:13:51 -0300

    Adding push on suggestion select in build search

--- lib/modules/search/presentation/widgets/organisms/champion_search_field.dart
+++ lib/modules/search/presentation/widgets/organisms/champion_search_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:from_css_color/from_css_color.dart';
+import 'package:riven/modules/builds/presentation/views/build.dart';
 import 'package:riven/modules/search/presentation/widgets/molecules/champion_search_tile.dart';
 import 'package:riven/modules/search/presentation/widgets/params/champion_search_field.dart';
 import 'package:riven/modules/search/presentation/widgets/params/champion_search_tile.dart';
@@ -53,7 +54,8 @@ class ChampionSearchField extends StatelessWidget {
         );
       },
       onSuggestionSelected: (Build suggestion) {
-        print(suggestion.champion!.name);
+        Navigator.of(context)
+            .pushNamed(BuildScreen.route, arguments: suggestion);
       },
     );
   }
